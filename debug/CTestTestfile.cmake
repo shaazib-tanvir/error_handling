ef24141bc57e17ef938c8d885056703fcfaa6173
Shaazib Tanvir <syed2302995@st.jmi.ac.in>
2024-07-26 18:53:27 +0530

    .gitignore update

--- debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/frisk17/c/error_handling/src
-# Build directory: /home/frisk17/c/error_handling/debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_error_debug "/home/frisk17/c/error_handling/debug/test_error_debug")
-set_tests_properties(test_error_debug PROPERTIES  _BACKTRACE_TRIPLES "/home/frisk17/c/error_handling/src/CMakeLists.txt;12;add_test;/home/frisk17/c/error_handling/src/CMakeLists.txt;0;")
